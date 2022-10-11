888107ad50dacc4b4d1829204dede1a0ab3e4b00
David Ward <davidmward@mail.com>
2022-10-10 17:39:29 -0700

    condensed stringLength()

--- C_Prog_Modern/strings/idioms.c
+++ C_Prog_Modern/strings/idioms.c
@@ -21,20 +21,29 @@ int readline(char input[], int maxChars)
 
 size_t stringLength(const char* s)
 {
-	size_t n = 0;
-	for (n = 0; *s != '\0'; s++)
+	//Note that the contents of s cannot be altered because it is constant.
+	
+	size_t count = 0;
+	
+	// Check if the current value of s is '\0', which is the same as 0 (false).
+	// After the check, increment s.
+	while (*s++)
 	{
-		n++;
+		count++;
 	}
-	return n;	
+	return count;	
 }
 
 int main(void)
 {
+	// Initialize
 	char string[MAX_LENGTH];
-	int linesRead;
+	
+	// Get input
 	printf("$ ");
-	linesRead = readline(string, MAX_LENGTH);
+	readline(string, MAX_LENGTH);
+
+	// Output
 	printf("$ %s\n", string);
 	printf("$ length: %lu\n", stringLength(string));
 
