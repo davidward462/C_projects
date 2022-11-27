860220b595d6db5a74ffe93bf01910cd23a41669
David Ward <davidmward@mail.com>
2022-11-26 20:49:46 -0800

    removed test cases

--- practice/stack/program.c
+++ practice/stack/program.c
@@ -2,25 +2,7 @@
 #include <stdbool.>
 #include "stack.c"
 
-void TestIsEmpty()
-{
-    return;
-}
 
-void TestIsFull()
-{
-    return;
-}
-
-void TestPush()
-{
-    return;
-}
-
-void TestPop()
-{
-    return;
-}
 
 int main()
 {
